47e4ec98af597b8b510febeed32534789ed30d54
alyoshaswamy <raghav.ravishankar11@gmail.com>
2024-01-18 00:00:47 +0530

    LFGGGG

---
+++ web.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import requests
+import json
+
+st.set_page_config(
+    page_title="TENSOIC - Kan-LLaMA",
+    page_icon="favicon.png",
+)
+st.title(":orange[Kan-LLaMA]")
+st.header("", divider="rainbow")
+
+
+
+# User input
+user_prompt = st.text_input("Enter your prompt:", placeholder="Hello, what can you do for me...")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+bearer_token = st.secrets["secrets"]["bearer_token"]
+
+
+# Function to send a request to the OpenAI endpoint
+def query_openai(prompt):
+    url = "https://api.xylem.ai/api/v0/completions"
+    headers = {
+        "Authorization": f"Bearer {bearer_token}",
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "model": "Kan-Llama-7B",
+        "prompt": prompt,
+        "temperature": 0.9,
+        "max_tokens": 200,
+
+    })
+    response = requests.post(url, headers=headers, data=data)
+    return response.json()
+
+# Button to send the prompt
+# Button to send the prompt
+if st.button("Send"):
+    if user_prompt:
+        # Concatenate all previous messages to form the conversation history
+        conversation_history = " ".join([message["content"] for message in st.session_state.messages])
+        full_prompt = conversation_history + " " + user_prompt
+
+        try:
+            # Making the API call with the full conversation history
+            response = query_openai(full_prompt)
+
+            # Update session state with the user's prompt and model's response
+            st.session_state.messages.append({"role": "user", "content": user_prompt})
+            response_text = response['choices'][0]['text']
+            st.session_state.messages.append({"role": "assistant", "content": response_text})
+
+        except Exception as e:
+            st.error(f"API request error: {e}")
+    else:
+        st.warning("Please enter a prompt.")
+
+# Display conversation history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
